fb848d44cc409989bf2e767c32e68a47485b68c5
Bashar AL MOHAMMAD <bash.mh.99@gmail.com>
2025-07-09 21:54:51 +0300

    add interactiviy

--- data/cart.js
+++ data/cart.js
@@ -49,3 +49,17 @@ export function removeFromCart(productId) {
 function saveToStorage () {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
+
+
+export function updateDeliveryOption (productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+      if (productId == cartItem.productId)
+          matchingItem = cartItem;
+  });
+
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
