bc70f2cf047f25717330b3656fd3abc7b2bbd6aa
Bashar AL MOHAMMAD <bash.mh.99@gmail.com>
2025-07-10 23:07:53 +0300

    private stuff

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart {
     cartItems;
-    localStorageKey;
+    #localStorageKey; // # means private property
 
     constructor(localStorageKey) {
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
     }
 
-    loadFromStorage () {
-        this.cartItems = JSON.parse(localStorage.getItem(`cart-${this.localStorageKey}`)) || [
+    #loadFromStorage () {
+        this.cartItems = JSON.parse(localStorage.getItem(`cart-${this.#localStorageKey}`)) || [
             {
                 productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
                 quantity: 2,
@@ -23,7 +23,7 @@ class Cart {
     }
 
     saveToStorage () {
-        localStorage.setItem(`cart-${this.localStorageKey}`, JSON.stringify(this.cartItems));
+        localStorage.setItem(`cart-${this.#localStorageKey}`, JSON.stringify(this.cartItems));
     }
     
     addToCart (productId) {
@@ -74,9 +74,6 @@ class Cart {
 
 const cart = new Cart('oop');
 const businessCart = new Cart('business');
-
-
-businessCart.loadFromStorage();
 
 console.log(cart);
 console.log(businessCart);
