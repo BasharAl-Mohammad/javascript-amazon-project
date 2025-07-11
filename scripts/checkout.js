7e46463e6c55a43cbf57bd7295f6b8558d29d9b0
Bashar AL MOHAMMAD <bash.mh.99@gmail.com>
2025-07-12 02:16:31 +0300

    async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -9,6 +9,23 @@ import { loadCart } from '../data/cart.js';
 
 
 
+
+
+async function loadPage() {
+    await loadProductsFetch();
+    const value = await new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value3');
+        });
+    });
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+
+loadPage();
+
+
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve) => {
@@ -21,7 +38,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
-
+*/
 
 
 /*
